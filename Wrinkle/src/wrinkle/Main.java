c1daf7e5cda80527fc6ffca2d2ff137de0fbe337
unknown <tastemyhouse@gmail.com>
2011-02-08 19:33:11 -0500

    test

--- Wrinkle/src/wrinkle/Main.java
+++ Wrinkle/src/wrinkle/Main.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-//A WET CUNT
+//A WET CUNT IS IN AN ELEPHANT
 package wrinkle;
 //import java.util.Random;
 import java.awt.*;
